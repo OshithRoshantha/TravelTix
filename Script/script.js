b4715b4df0593b9fc8f0fac167de8bc8655b34f4
Oshith Roshantha <oedirisuriya@gmail.com>
2024-05-08 10:33:59 +0530

    contactUs

--- Script/script.js
+++ Script/script.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+function welcomeTyping() {
     var element = document.querySelector('.welcomeText');
     var text = element.innerHTML;
     element.innerHTML = '';
@@ -13,4 +13,32 @@ window.onload = function() {
       }
     }, 100); 
   }
-  
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.header');
+    var scrollAmount = window.scrollY;
+    var windowHeight = window.innerHeight;
+    var documentHeight = document.body.scrollHeight;
+
+    if (scrollAmount > 40) { 
+        header.classList.add('visible');
+    } else {
+        header.classList.remove('visible');
+    }
+    if (scrollAmount === 0 || scrollAmount + windowHeight >= documentHeight) {
+      header.classList.remove('visible');
+    }
+});
+function scrollToEnd() {
+  const docHeight = document.body.scrollHeight;
+  const scrollDistance = docHeight - window.innerHeight;
+  window.scrollTo({
+      top: scrollDistance,
+      behavior: 'smooth'
+  });
+}
+function scrollToTop() {
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+  });
+}
