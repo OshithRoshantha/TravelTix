f58aca48f81600102da7d7682ea5cda2b6752020
Oshith Roshantha <oedirisuriya@gmail.com>
2024-05-10 16:32:07 +0530

    backEnd

--- Script/script3.js
+++ Script/script3.js
@@ -4,3 +4,18 @@ document.querySelector('.switcher').addEventListener('click', function() {
     document.querySelector('.fromBar').value = toInput;
     document.querySelector('.toBar').value = fromInput;
 });
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    var timeString = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+    document.getElementById('currentTime').innerText = timeString;
+}
+setInterval(updateTime, 1000);
